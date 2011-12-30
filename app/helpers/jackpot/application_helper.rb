68a94a003ee1895eb870c0cf7c5de982e785fa12
Vitor Pellegrino <vitorp@gmail.com>
2011-12-30 00:23:59 -0200

    Bootstrap alerts for success messages

--- app/helpers/jackpot/application_helper.rb
+++ app/helpers/jackpot/application_helper.rb
@@ -1,4 +1,18 @@
 module Jackpot
   module ApplicationHelper
+    def twitterized_type(type)
+      case type
+      when :alert
+        "warning"
+      when :error
+        "error"
+      when :notice
+        "success"
+      when :info
+        "info"
+      else
+        type.to_s
+      end
+    end
   end
 end
