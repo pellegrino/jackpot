c217ef4af4d53d541b76372e150979a7d08fe349
Vitor Pellegrino <vitorp@gmail.com>
2012-01-02 20:13:15 -0200

    Navbar entry method

--- app/helpers/jackpot/application_helper.rb
+++ app/helpers/jackpot/application_helper.rb
@@ -14,5 +14,14 @@ module Jackpot
         type.to_s
       end
     end
+
+    def navbar_entry(name, options = {}, html_options = {}, &block)
+      if current_page?(options) 
+        content_tag("li", class: "active")  { link_to(name, options, html_options, &block) } 
+      else
+        content_tag("li")                   { link_to(name, options, html_options, &block) }
+      end 
+    end 
   end
+
 end
