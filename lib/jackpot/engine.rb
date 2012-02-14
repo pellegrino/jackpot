27e01d16b3c5c295e597d32c568b942c01377b7a
Vitor Pellegrino <vitorp@gmail.com>
2012-02-13 23:20:57 -0200

    Removing haml dependency

--- lib/jackpot/engine.rb
+++ lib/jackpot/engine.rb
@@ -1,4 +1,3 @@
-require 'haml'
 require 'active_merchant'
 require 'devise'
 require 'formtastic-bootstrap'
@@ -9,7 +8,6 @@ module Jackpot
     config.generators do |g|
       g.test_framework      :rspec, :view_specs => false
       g.fixture_replacement :factory_girl, :dir => "spec/support/factories" 
-      g.template_engine     :haml
     end
   end
 end
