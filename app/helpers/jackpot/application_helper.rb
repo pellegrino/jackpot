32fcc8599a8fe87fe231bbb552c810883b870be9
Vitor Pellegrino <vitorp@gmail.com>
2012-03-27 10:41:35 -0300

    Adds sidebar containing jackpot configuration info

--- app/helpers/jackpot/application_helper.rb
+++ app/helpers/jackpot/application_helper.rb
@@ -22,6 +22,24 @@ module Jackpot
         content_tag("li")                   { link_to(name, options, html_options, &block) }
       end 
     end 
+
+
+    def gravatar_for_current_user options = {}
+      options = {:alt => 'avatar', :class => 'avatar', :size => 80}.merge! options
+      id = Digest::MD5::hexdigest current_user.email.strip.downcase
+      url = 'http://www.gravatar.com/avatar/' + id + '.jpg?s=' + options[:size].to_s
+      options.delete :size
+      image_tag url, options
+    end
+
+    def jackpot_gateway
+      Jackpot.configuration.gateway["type"].to_s.humanize
+    end 
+    def jackpot_gateway_mode
+      Jackpot.configuration.gateway["mode"].to_s.humanize
+    end 
+
   end
+
 
 end
