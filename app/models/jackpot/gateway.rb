71f2e271757af5eb4a58e981f7c0ca0e79f1fe33
Vitor Pellegrino <vitorp@gmail.com>
2012-04-03 18:00:57 -0300

    refatoring gateway adaptee instance name

--- app/models/jackpot/gateway.rb
+++ app/models/jackpot/gateway.rb
@@ -3,22 +3,22 @@ module Jackpot
   # Small adapter for Active Merchant gateways Jackpot supports
   class Gateway
 
-    def initialize(gateway)
-      @gateway = gateway
+    def initialize(adapted_gateway)
+      @adapted_gateway = adapted_gateway
     end 
 
 
     def authorize(amount_in_cents, credit_card_token) 
-      @gateway.authorize(amount_in_cents, credit_card_token)
+      @adapted_gateway.authorize(amount_in_cents, credit_card_token)
     end 
 
 
     def store(card)
-      @gateway.store(card)
+      @adapted_gateway.store(card)
     end 
 
     def capture(money, authorization, options = {})
-      @gateway.capture(money, authorization, options) 
+      @adapted_gateway.capture(money, authorization, options) 
     end 
 
   end 
